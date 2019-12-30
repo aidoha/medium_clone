00e6f856948712110071552ef502512bebc0cc0f
Айдын Ибраев (Aydyn Ibraev) <AIbraev@alfabank.kz>
2019-12-30 17:19:40 +0600

    Structure, router, sign in page

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <div className="App">HOOKs</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Routes from 'routes';
+import TopBar from 'components/topBar';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
