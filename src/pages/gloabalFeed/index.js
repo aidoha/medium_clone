392499977b1b92514fa8a2983330b52adf47b107
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2020-01-08 00:08:38 +0600

    pagination

--- src/pages/gloabalFeed/index.js
+++ src/pages/gloabalFeed/index.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
+import { stringify } from 'query-string';
 
 import useFetch from '../../hooks/useFetch';
 import Feed from '../../components/feed';
+import Pagination from '../../components/pagination';
+import { getPaginator, limit } from '../../utils';
 
-const GlobalFeed = () => {
-	const apiUrl = '/articles?limit=20&offset=0';
+const GlobalFeed = ({ location, match }) => {
+	const { currentPage, offset } = getPaginator(location.search);
+	const stringifiedParams = stringify({ limit, offset });
+	const apiUrl = `/articles?${stringifiedParams}`;
 	const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+	const url = match.url;
 
 	useEffect(() => {
 		doFetch();
-	}, [doFetch]);
+	}, [doFetch, currentPage]);
 
 	return (
 		<div className="home-page">
@@ -24,7 +30,17 @@ const GlobalFeed = () => {
 					<div className="col-md-9">
 						{isLoading && <div>Loading...</div>}
 						{error && <div>Some error happened</div>}
-						{response && <Feed articles={response.articles} />}
+						{response && !isLoading && (
+							<>
+								<Feed articles={response.articles} />
+								<Pagination
+									total={response.articlesCount}
+									url={url}
+									currentPage={currentPage}
+									limit={limit}
+								/>
+							</>
+						)}
 					</div>
 					<div className="col-md-3">Popular tags</div>
 				</div>
