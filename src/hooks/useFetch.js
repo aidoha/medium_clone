0657567cc74e44649bdae162ea000466725b331a
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2020-01-07 16:56:42 +0600

    get current user, ex deps

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
+import useLocalStorage from './useLocalStorage';
 
 export default url => {
 	const baseUrl = 'https://conduit.productionready.io/api';
@@ -7,17 +8,27 @@ export default url => {
 	const [response, setResponse] = useState(null);
 	const [error, setError] = useState(null);
 	const [options, setOptions] = useState({});
+	const [token] = useLocalStorage('token');
 
-	const doFetch = (options = {}) => {
+	const doFetch = useCallback((options = {}) => {
 		setOptions(options);
 		setIsLoading(true);
-	};
+	}, []);
 
 	useEffect(() => {
+		const requestOptions = {
+			...options,
+			...{
+				headers: {
+					authorization: token ? `Token ${token}` : '',
+				},
+			},
+		};
+
 		if (!isLoading) {
 			return;
 		}
-		axios(baseUrl + url, options)
+		axios(baseUrl + url, requestOptions)
 			.then(res => {
 				setIsLoading(false);
 				setResponse(res.data);
@@ -26,7 +37,7 @@ export default url => {
 				setIsLoading(false);
 				setError(error.response.data);
 			});
-	}, [isLoading, options, url]);
+	}, [isLoading, options, url, token]);
 
 	return [{ response, isLoading, error }, doFetch];
 };
